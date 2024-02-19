6b4364964e85a2aa6e1d65c615c8da1b0bab4379
Mayank Shrivastava <146957986+miracleicon@users.noreply.github.com>
2024-02-19 19:45:01 +0530

    Greatestnumof3digit.c

---
+++ Greatestnumof3digit.c
@@ -0,0 +1,20 @@
+//Write a program to find out the greatest number out of three numbers. 
+#include<stdio.h>
+int main()
+{
+int n1, n2 , n3;
+printf("Enter Three Digit number: ");
+scanf("%d %d %d", &n1, &n2, &n3);
+if (n1 >= n2 && n1 >= n3) 
+{
+        printf("The greatest number is: %d", n1);
+    } 
+    else if (n2 >= n1 && n2 >= n3) 
+    {
+        printf("The greatest number is: %d", n2);
+    } else 
+    {
+        printf("The greatest number is: %d", n3);
+    }
+return 0;
+}
